8af1a41a137b5db0bcfd68d5c99e1fee147b6af6
Phil Fenstermacher <pcfens@wm.edu>
2017-08-03 17:32:25 -0400

    Modify XML settings at runtime

--- Dockerfile
+++ Dockerfile
@@ -13,8 +13,7 @@ LABEL org.label-schema.vendor="Nix Team <unix@lists.wm.edu>" \
       org.label-schema.schema-version="1.0"
 
 # Configure Tomcat
-ENV CATALINA_OPTS="-server -Xms2g -Xmx4g -XX:MaxPermSize=512m" \
-    BANNER_INSTANCE="DEVL"
+ENV CATALINA_OPTS="-server -Xms2g -Xmx4g -XX:MaxPermSize=512m"
 
 COPY tomcat_config/* /usr/local/tomcat/conf/
 COPY banner_config/* /banner_config/
@@ -22,10 +21,12 @@ COPY banner_config/* /banner_config/
 ADD https://developer.byu.edu/maven2/content/groups/thirdparty/com/oracle/ojdbc6/11.2.0.1.0/ojdbc6-11.2.0.1.0.jar /usr/local/tomcat/lib/
 ADD https://developer.byu.edu/maven2/content/groups/thirdparty/com/oracle/xdb6/11.2.0.4/xdb6-11.2.0.4.jar /usr/local/tomcat/lib/
 
-RUN rm -Rf /usr/local/tomcat/webapps/* \
+RUN apk add --no-cache xmlstarlet \
+    && rm -Rf /usr/local/tomcat/webapps/* \
     && apk add --no-cache tzdata \
     && cp /usr/share/zoneinfo/America/New_York /etc/localtime
 
 COPY run.sh /usr/local/tomcat/bin/
+COPY extract-war.sh /usr/local/bin/extract-war.sh
 
 CMD ["/usr/local/tomcat/bin/run.sh"]
--- run.sh
+++ run.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 # shellcheck disable=SC2013,SC2046
 
-if [ -z "$BANNER_INSTANCE" ]; then
-  echo 'ERROR: BANNER_INSTANCE must be set'
-  exit 1
-fi
-
 PROPFILE="/usr/local/tomcat/conf/catalina.properties"
 if [ ! -f "$PROPFILE" ]; then
   echo "Unable to find properties file $PROPFILE"
@@ -15,6 +10,18 @@ fi
 setProperty() {
   prop=$1
   val=$2
+
+  if [ "$prop" = "cas.serverName" ]; then
+    for settings_file in /usr/local/tomcat/webapps/*/WEB-INF/web.xml; do
+      xml ed  --inplace -N x="http://java.sun.com/xml/ns/javaee" -u "/x:web-app/x:filter[x:filter-name[normalize-space(text())='CAS Validation Filter']]/x:init-param[x:param-name[normalize-space(text())='serverName']]/x:param-value" -v "$val" "$settings_file"
+    done
+  fi
+
+  if [ "$prop" = "cas.casServerUrlPrefix" ]; then
+    for settings_file in /usr/local/tomcat/webapps/*/WEB-INF/web.xml; do
+      xml ed  --inplace -N x="http://java.sun.com/xml/ns/javaee" -u "/x:web-app/x:filter[x:filter-name[normalize-space(text())='CAS Validation Filter']]/x:init-param[x:param-name[normalize-space(text())='casServerUrlPrefix']]/x:param-value" -v "$val" "$settings_file"
+    done
+  fi
 
   if [ $(grep -c "$prop" "$PROPFILE") -eq 0 ]; then
     echo "${prop}=$val" >> "$PROPFILE"
@@ -33,23 +40,9 @@ setPropsFromFile() {
   done
 }
 
-setPropFromEnv() {
-  prop=$1
-  val=$2
-  # If no value was given, abort
-  [ -z "$val" ] && return
-  if [ $(grep -c "$prop" "$PROPFILE") -eq 0 ]; then
-    echo "${prop}=$val" >> "$PROPFILE"
-  else
-    val=$(echo "$val" |sed 's#/#\\/#g')
-    sed -i "s/$prop=.*/$prop=$val/" "$PROPFILE"
-  fi
-}
-
-if [ -f "/opt/instance-config/${BANNER_INSTANCE}.properties" ]; then
-  setPropsFromFile /opt/instance-config/${BANNER_INSTANCE}.properties
+if [ -z "$CONFIG_FILE" ] && [ -f "$CONFIG_FILE" ]; then
+    setPropsFromFile "$CONFIG_FILE"
 fi
-
 
 # Pull properties from docker secrets
 if [ -d /run/secrets ]; then
@@ -60,10 +53,11 @@ if [ -d /run/secrets ]; then
   done
 fi
 
-setPropFromEnv bannerdb.url "$BANNERDB_URL"
-setPropFromEnv banproxy.username "$BANNERDB_USER"
-setPropFromEnv banproxy.password "$BANNERDB_PASSWORD"
-setPropFromEnv banssuser.username "$BANNERSSDB_USER"
-setPropFromEnv banssuser.password "$BANNERSSDB_PASSWORD"
+setProperty bannerdb.url "$BANNERDB_URL"
+setProperty banproxy.username "$BANNERDB_USER"
+setProperty banproxy.password "$BANNERDB_PASSWORD"
+setProperty banssuser.username "$BANNERSSDB_USER"
+setProperty banssuser.password "$BANNERSSDB_PASSWORD"
+setProperty cas.serverName "$APP_URL"
 
 exec catalina.sh run
