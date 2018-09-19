654e305dcda3af2350dd670cb7ce610a3c83befe
Phil Fenstermacher <pcfens@wm.edu>
2018-09-19 10:54:13 -0400

    Remove old stuff

--- extract-war.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd /usr/local/tomcat/webapps
-
-for war in /usr/local/tomcat/webapps/*.war; do
-  mkdir $(basename "$war" .war)
-  unzip -d $(basename "$war" .war) "$war"
-  rm "$war"
-done
